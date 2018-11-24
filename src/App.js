8a8bf24682f6bcd7ed9ae26e650fe27180698b43
lahin31 <muhammad.lahin@gmail.com>
2018-11-24 15:58:35 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,28 +1,69 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import { fetchUsers } from "./api";
+import axios from "axios";
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+function App() {
+  const [dummyUsers, setDummyUsers] = useState([]);
+  const [customUsers, setCustomUsers] = useState(fetchUsers);
+  const [showCustomUsers, setShowCustomUsers] = useState(false);
+
+  // componentDidMount
+  useEffect(() => {
+    axios
+      .get("https://jsonplaceholder.typicode.com/users")
+      .then(resp => {
+        setDummyUsers(resp.data);
+        setShowCustomUsers(true);
+      })
+      .catch(() => {
+        setCustomUsers([]);
+        setShowCustomUsers(false);
+      });
+  }, []); //  Don't forget to put [] square brackets, which will help to run it only after the dom render(once)
+
+  const deleteDummyUsers = index => {
+    const _dummyUsers = [...dummyUsers];
+    _dummyUsers.splice(index, 1);
+    setDummyUsers(_dummyUsers);
+  };
+
+  const deleteCustomUser = index => {
+    const _customUsers = [...customUsers];
+    _customUsers.splice(index, 1);
+    setCustomUsers(_customUsers);
+  };
+  return (
+    <div>
+      <h4>From DummyData(JsonPlaceholder)</h4>
+      {showCustomUsers === false && customUsers.length !== 0 ? (
+        <p>...loading</p>
+      ) : (
+        <ul>
+          {dummyUsers.map((dummyUser, i) => {
+            return (
+              <li key={dummyUser.id}>
+                <b>Name</b>:{dummyUser.name}
+                <b> ID</b>: {dummyUser.id}
+                <button onClick={() => deleteDummyUsers(i)}>Delete</button>
+              </li>
+            );
+          })}
+        </ul>
+      )}
+      <h4>From Custom Api</h4>
+      <ul>
+        {customUsers.map((customUser, i) => {
+          return (
+            <li key={customUser.id}>
+              <b>Name</b>:{customUser.name}
+              <b> ID</b>: {customUser.id}
+              <button onClick={() => deleteCustomUser(i)}>Delete</button>
+            </li>
+          );
+        })}
+      </ul>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/api.js
@@ -0,0 +1,21 @@
+const users = [
+  {
+    id: 1,
+    name: "John  Cena",
+    country: "USA"
+  },
+  {
+    id: 2,
+    name: "Brock Lesner",
+    country: "USA"
+  },
+  {
+    id: 3,
+    name: "Chris Jericho",
+    country: "Canada"
+  }
+];
+
+export const fetchUsers = () => {
+  return users;
+};
